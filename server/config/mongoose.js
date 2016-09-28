4a43ece211a433a54df963c272c2c0205422d1a4
AnuarBJJ <anuar.zhukenov@gmail.com>
2016-09-28 13:07:20 -0700

    connecting to mLab

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -41,39 +41,4 @@ db.once('open', function callback () {
    */
   seventies.save();
 
-  /*
-   * Then we need to give Boyz II Men credit for their contribution
-   * to the hit "One Sweet Day".
-   */
-
- });
-
-
-/*
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-
-var fs = require('fs');
-
-var path = require('path');
-
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/core';
-
-mongoose.connect(uristring, function (err, res) {
-  if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-  console.log ('Succeeded connected to: ' + uristring);
-  }
 });
-
-var models_path = path.join(__dirname, './../models')
-
-fs.readdirSync(models_path).forEach(function(file) {
-  if(file.indexOf('.js') >= 0) {
-    // require the file (this runs the model file which registers the schema)
-    require(models_path + '/' + file);
-  }
-});
-*/
